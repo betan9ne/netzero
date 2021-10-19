a2d141dcd38c9fa8959ad07d5dc7e2b86ec765bd
betan9ne <chisomo9@gmail.com>
2021-10-19 16:45:28 +0000

    first commit

---
+++ netzero/src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/storage"
+import "firebase/auth"
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXsOcJkULtPP4sonOFyKkOkDsznZJfphM",
+  authDomain: "netzero-a417f.firebaseapp.com",
+  projectId: "netzero-a417f",
+  storageBucket: "netzero-a417f.appspot.com",
+  messagingSenderId: "378417421321",
+  appId: "1:378417421321:web:baf0c52f92d114936eacd8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
